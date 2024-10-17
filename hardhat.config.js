c3ebe960602d56f670ffe923da18bdeb26fdc8e6
ymohit1603 <mohityadav0330@gmail.com>
2024-10-17 09:34:27 +0530

    add: reading secret with fs module

---
+++ hardhat.config.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+
+require("@nomicfoundation/hardhat-toolbox");
+
+
+const privateKey = fs.readFileSync('.secret').toString().trim();
+
+
+module.exports = {
+  networks: {
+    hardhat: {
+      chainId:1337,
+    },
+  },
+  solidity: "0.8.27",
+};
